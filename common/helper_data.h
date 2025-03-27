e1d48486b0bc8d42f03e521a08b55e3b02115ca0
lmyybh <lmyybh_lazy@163.com>
2025-03-27 22:12:06 +0800

    feat(common): add helper_cuda.cuh and helper_data.h to common directory

---
+++ common/helper_data.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void initialRangeData(float* p, const int size, float start, float step) {
+  for (int i = 0; i < size; ++i) {
+    p[i] = start + step * i;
+  }
+}
+
+bool checkResult(float* hostRef, float* gpuRef, const int N) {
+  double eps = 1.0E-8;
+  bool match = true;
+  for (int i = 0; i < N; ++i) {
+    if (abs(hostRef[i] - gpuRef[i]) > eps) {
+      match = false;
+      break;
+    }
+  }
+
+  return match;
+}
