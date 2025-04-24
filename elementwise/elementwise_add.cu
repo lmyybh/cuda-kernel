d456469e49d8c32dd2a3eaf60e93c2c6e1f01d57
lmyybh <lmyybh_lazy@163.com>
2025-04-24 09:58:55 +0800

    docs(elementwise): add comments in elementwise_add.cu

--- elementwise/elementwise_add.cu
+++ elementwise/elementwise_add.cu
@@ -2,18 +2,23 @@
 
 #define FLOAT4(value) (reinterpret_cast<float4*>(&(value))[0])
 
+// 朴素实现：每个 thread 负责一个元素的加法运算
 __global__ void elementwise_add(float* A, float* B, float* C, const int N) {
   int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (tid < N) { C[tid] = A[tid] + B[tid]; }
 }
 
+// Grid-Stride Loops 网格跨步循环：每个 thread 可以负责多个元素运算，间隔为总的 thread 数量
+// 优点：wrap 内 thread 的内存访问连续；可以使用任意数量的 thread 完成 N 个元素运算
 __global__ void elementwise_add_gsl(float* A, float* B, float* C, const int N) {
   for (int i = threadIdx.x + blockIdx.x * blockDim.x; i < N; i += blockDim.x * gridDim.x) {
     C[i] = A[i] + B[i];
   }
 }
 
+// Vectorized Memory Access 向量化访存：使用一条 LD.E.128 指令代替四条 LD.E 指令，
+// 优点：可以一次读取 4 个 float 数据，降低延迟，提高带宽
 __global__ void elementwise_add_vec4(float* A, float* B, float* C, const int N) {
   int index = 4 * (threadIdx.x + blockIdx.x * blockDim.x);
 
@@ -35,6 +40,7 @@ __global__ void elementwise_add_vec4(float* A, float* B, float* C, const int N) 
   }
 }
 
+// 向量化访存 + 网格跨步循环
 __global__ void elementwise_add_vec4_gsl(float* A, float* B, float* C, const int N) {
   int index = 4 * (threadIdx.x + blockIdx.x * blockDim.x);
 
@@ -56,10 +62,12 @@ __global__ void elementwise_add_vec4_gsl(float* A, float* B, float* C, const int
   }
 }
 
+// CPU 运算
 void call_add_f32_host(float* A, float* B, float* C, const int N) {
   for (int i = 0; i < N; ++i) { C[i] = A[i] + B[i]; }
 }
 
+// kernel 调用
 void call_add_f32_device(int whichKernel, int blockSize, float* d_A, float* d_B, float* d_C,
                          const int N) {
   void (*kernel)(float*, float*, float*, const int);
